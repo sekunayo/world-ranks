2079be120e68100bb417291237232e3324cf963b
sekunayo <graceokereke22@gmail.com>
2022-05-31 20:24:18 +0100

    updates

--- pages/world-ranks/index.tsx
+++ pages/world-ranks/index.tsx
@@ -2,21 +2,57 @@ import React, { useEffect, useState } from "react";
 import Icon from "../../components/Icon/icon";
 import { Table } from "../../components/Table";
 import { fetchCountries } from "../../lib/apis";
+import { BiSearchAlt } from "react-icons/bi";
 import styles from "./world-ranks.module.scss";
 
 const WorldRanks = () => {
   const [searchValue, setSearchValue] = useState("");
   const [countries, setCountries] = useState<string[]>([]);
 
+  const filterCountries = () => {
+    const newCountries = countries?.map((element: any) => {
+      if (
+        element?.name?.common
+          .toLowerCase()
+          .includes(searchValue.toLowerCase()) ||
+        element?.region?.toLowerCase().includes(searchValue.toLowerCase()) ||
+        element?.subregion?.toLowerCase().includes(searchValue.toLowerCase())
+      ) {
+        return element;
+      }
+    });
+
+    const notRelatedCountries = countries?.map((element: any) => {
+      if (
+        !element?.name?.common
+          .toLowerCase()
+          .includes(searchValue.toLowerCase()) ||
+        !element?.region?.toLowerCase().includes(searchValue.toLowerCase()) ||
+        !element?.subregion?.toLowerCase().includes(searchValue.toLowerCase())
+      ) {
+        return element;
+      }
+    });
+
+    return [newCountries, notRelatedCountries];
+  };
+
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // const newCountry = filterCountries();
+
+    // setCountries(newCountry);
   };
 
   useEffect(() => {
     fetchCountries().then((data) => {
       setCountries(data);
     });
-  },[fetchCountries]);
+  }, [fetchCountries]);
 
   return (
     <div className={styles["worldRanks"]}>
@@ -32,7 +68,13 @@ const WorldRanks = () => {
             Found {countries?.length} countries
           </p>
 
-          <form className={styles["worldRanks__search-form"]}>
+          <form
+            onSubmit={(e) => handleSubmit(e)}
+            className={styles["worldRanks__search-form"]}
+          >
+            <div className={styles["worldRanks__search-icon"]}>
+              <BiSearchAlt />
+            </div>
             <input
               value={searchValue}
               onChange={(e) => handleSearchChange(e)}
